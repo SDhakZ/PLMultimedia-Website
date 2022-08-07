3217ed67b2239bfff57b150357627a98b2759d7b
SDhakZ <sharnam34@gmail.com>
2022-08-07 20:31:09 +0545

    First

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./components/Pages/Home";
+import { About } from "./components/Pages/About";
+import { Blog } from "./components/Pages/Blog";
+import { Contact } from "./components/Pages/Contact";
+import { Services } from "./components/Pages/Services";
+import { Portfolio } from "./components/Pages/Portfolio";
+function App() {
+  return (
+    <>
+      <Router>
+        <NavBar />
+        <div className="pages">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/blog" component={Blog} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/services" component={Services} />
+            <Route path="/portfolio" component={Portfolio} />
+          </Switch>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
