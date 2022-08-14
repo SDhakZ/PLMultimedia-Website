3e9d43547a6d1d0430a312a010ca0d22813aa1bc
SDhakZ <sharnam34@gmail.com>
2022-08-14 18:35:05 +0545

    "background tto contact css animation"

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -4,6 +4,14 @@ export const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <div class="container">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+      </div>
     </div>
   );
 };
