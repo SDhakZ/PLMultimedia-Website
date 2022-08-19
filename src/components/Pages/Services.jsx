6b0f908180fea0d4c8e4268e0b8b90685482c7ba
SDhakZ <sharnam34@gmail.com>
2022-08-19 18:01:12 +0545

    ".default added"

--- src/components/Pages/Services.jsx
+++ src/components/Pages/Services.jsx
@@ -4,47 +4,47 @@ Service components to form the Services page for PL Multimedia*/
 import React from "react";
 import { ServiceCard, MoreCard } from "../ServiceCard.jsx";
 import ServicesCSS from "../CSSmodules/Services.module.css";
-import DomainSearch  from "../DomainSearch.jsx";
+import DomainSearch from "../DomainSearch.jsx";
 
 function Services() {
   return (
     <div className="ServicesPage">
-      <DomainSearch/>
+      <DomainSearch />
 
       <div className={ServicesCSS["services-container"]}>
         <div className={ServicesCSS.serviceCardSection}>
           <ServiceCard
-            logo={require("../../logo/advertisement.png")}
+            logo={require("../../logo/advertisement.png").default}
             title="Advertisement"
             description="lorem20 is not working on my vs code so i am randomly typing this paragraph. And how is the weather there in kathmandu?"
           />
           <ServiceCard
-            logo={require("../../logo/webDevelopment.png")}
+            logo={require("../../logo/webDevelopment.png").default}
             title="Web Development"
             description="lorem20 is not working on my vs code so i am randomly typing this paragraph. And how is the weather there in kathmandu?"
           />
           <ServiceCard
-            logo={require("../../logo/webHosting.png")}
+            logo={require("../../logo/webHosting.png").default}
             title="Web Hosting"
             description="lorem20 is not working on my vs code so i am randomly typing this paragraph. And how is the weather there in kathmandu?"
           />
           <ServiceCard
-            logo={require("../../logo/mobileAppDevelopment.png")}
+            logo={require("../../logo/mobileAppDevelopment.png").default}
             title="Mobile App Development"
             description="lorem20 is not working on my vs code so i am randomly typing this paragraph. And how is the weather there in kathmandu?"
           />
           <ServiceCard
-            logo={require("../../logo/seo.png")}
+            logo={require("../../logo/seo.png").default}
             title="SEO Optimization"
             description="lorem20 is not working on my vs code so i am randomly typing this paragraph. And how is the weather there in kathmandu?"
           />
           <ServiceCard
-            logo={require("../../logo/digitalMarketing.png")}
+            logo={require("../../logo/digitalMarketing.png").default}
             title="Digital Marketing"
             description="lorem20 is not working on my vs code so i am randomly typing this paragraph. And how is the weather there in kathmandu?"
           />
           <ServiceCard
-            logo={require("../../logo/mediaProduction.png")}
+            logo={require("../../logo/mediaProduction.png").default}
             title="Media Production"
             description="lorem20 is not working on my vs code so i am randomly typing this paragraph. And how is the weather there in kathmandu?"
           />
