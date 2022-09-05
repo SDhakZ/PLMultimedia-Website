2d81dbf2a4c6c6a43ea9fca8d3b88a09fbc21337
SDhakZ <sharnam34@gmail.com>
2022-09-05 13:29:20 +0545

    "Rest API connected intern"

--- src/components/InternCard.jsx
+++ src/components/InternCard.jsx
@@ -2,27 +2,26 @@ import React from "react";
 import InternsCSS from "../components/CSSmodules/interns.module.css";
 function InternCard(props) {
   return (
-    <>
-      <div className={InternsCSS["intern-card"]}>
-        <div className={InternsCSS["intern-lft"]}>
-          <figure className={InternsCSS["interns-picContainer"]}>
-            <img
-              className={InternsCSS["interns-picture"]}
-              src={props.img}
-            ></img>
-          </figure>
-          <div className={InternsCSS["interns-detailsContainer"]}>
-            <h2 className={InternsCSS["intern-name"]}>{props.name}</h2>
-            <div className={InternsCSS["interns-detail"]}>
-              <p>Company: {props.company}</p>
-              <p>Field: {props.field}</p>
-              <p>Position: {props.position}</p>
-            </div>
+    <div className={InternsCSS["intern-card"]}>
+      <div className={InternsCSS["intern-lft"]}>
+        <figure className={InternsCSS["interns-picContainer"]}>
+          <img
+            className={InternsCSS["interns-picture"]}
+            src={props.img}
+            alt="image"
+          ></img>
+        </figure>
+        <div className={InternsCSS["interns-detailsContainer"]}>
+          <h2 className={InternsCSS["intern-name"]}>{props.name}</h2>
+          <div className={InternsCSS["interns-detail"]}>
+            <p>Company: {props.company}</p>
+            <p>Field: {props.field}</p>
+            <p>Position: {props.position}</p>
           </div>
         </div>
-        <h3 className={InternsCSS["intern-date"]}>Year: {props.year}</h3>
       </div>
-    </>
+      <h3 className={InternsCSS["intern-date"]}>Year: {props.year}</h3>
+    </div>
   );
 }
 
--- src/components/Pages/Interns.jsx
+++ src/components/Pages/Interns.jsx
@@ -3,23 +3,32 @@ import MainHeading from "../MainHeading";
 import InternsCSS from "../CSSmodules/interns.module.css";
 import InternCard from "../InternCard";
 import internsData from "../../Datas/internsData";
+import useFetch from "../../hooks/useFetch";
 
 function Interns() {
+  const { loading, error, data } = useFetch(
+    "http://localhost:1338/api/interns?populate=*"
+  );
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+
   return (
     <React.Fragment>
       <div className={InternsCSS["interns-container"]}>
         <MainHeading headingName="Interns Through Us" />
         <div className={InternsCSS["interns-flexbox"]}>
-          {internsData.map((intern) => {
+          {data.data.map((intern) => {
+            const url = intern.attributes.img.data.attributes.url;
+            const image = "http://localhost:1338" + url;
             return (
               <InternCard
-                key={intern.id}
-                img={intern.img}
-                name={intern.name}
-                company={intern.company}
-                field={intern.field}
-                position={intern.position}
-                year={intern.year}
+                key={intern.attributes.id}
+                img={image}
+                name={intern.attributes.name}
+                company={intern.attributes.company}
+                field={intern.attributes.field}
+                position={intern.attributes.position}
+                year={intern.attributes.date}
               />
             );
           })}
