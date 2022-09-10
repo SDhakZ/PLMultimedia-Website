8ad0931cb6598dc6dc5fbdd7bfb7ae9c48587d9a
SDhakZ <sharnam34@gmail.com>
2022-09-10 18:29:19 +0545

    "Footer fix"

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -4,9 +4,9 @@ import LoadingPageCSS from "../components/CSSmodules/loadingPage.module.css";
 function LoadingPage() {
   return (
     <div className={LoadingPageCSS["LoadingPage-container"]}>
-      {/* <div className={LoadingPageCSS["LoadingPage-outer"]}>
-                <div className={LoadingPageCSS["LoadingPage-inner"]}/>
-            </div> */}
+      {/*    <div className={LoadingPageCSS["LoadingPage-outer"]}>
+        <div className={LoadingPageCSS["LoadingPage-inner"]} />
+      </div> */}
     </div>
   );
 }
