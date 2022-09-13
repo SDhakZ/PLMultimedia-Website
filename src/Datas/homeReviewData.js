fabe16c08a5978bec1b3d5bdf1699412bd7392b8
SDhakZ <sharnam34@gmail.com>
2022-09-13 10:27:43 +0545

    "Review add"

---
+++ src/Datas/homeReviewData.js
@@ -0,0 +1,24 @@
+const homecommentData = [
+  {
+    id: 1,
+    img: require("../images/stock.jpg"),
+    name: "Cameron Williamson",
+    comment:
+      "All-in-one place for engaging in remote team discussions for collaborating and also tracking progress. With your team you can achieve more efficiently being online.",
+  },
+  {
+    id: 2,
+    img: require("../images/stock2.jpg"),
+    name: "Desmond Dickinson",
+    comment:
+      "All-in-one place for engaging in remote team discussions for collaborating and also tracking progress. With your team you can achieve more efficiently being online.With your team you can achieve more efficiently being online.",
+  },
+  {
+    id: 3,
+    img: require("../images/altInternimg.jpg"),
+    name: "Kath Green",
+    comment:
+      "All-in-one place for engaging in remote team discussions for collaborating and also tracking progress. With your team you can achieve more efficiently being online.",
+  },
+];
+export default homecommentData;
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import HomeCSS from "../CSSmodules/home.module.css";
 import HomeTop from "../HomeComponents/homeTop";
 import HomeAboutUs from "../HomeComponents/homeAboutUs";
+import HomeReview from "../HomeComponents/homeReview";
 export const Home = () => {
   return (
     <div className={HomeCSS["Home-Container"]}>
@@ -14,7 +15,9 @@ export const Home = () => {
       <section className={HomeCSS["Home-services"]}></section>
       <section className={HomeCSS["Home-recent-works"]}></section>
       <section className={HomeCSS["Home-blogs"]}></section>
-      <section className={HomeCSS["Home-what-ppl-think"]}></section>
+      <section className={HomeCSS["Home-what-ppl-think"]}>
+        <HomeReview />
+      </section>
     </div>
   );
 };
