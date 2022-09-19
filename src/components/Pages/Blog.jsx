5e4225b9816fa664c6b5e728c193410c922fca48
SDhakZ <sharnam34@gmail.com>
2022-09-19 15:36:24 +0545

    "latest"

--- src/components/Pages/Blog.jsx
+++ src/components/Pages/Blog.jsx
@@ -3,47 +3,48 @@ import useFetch from "../../hooks/useFetch";
 import LoadingPage from "../LoadingPage";
 import ErrorPage from "../ErrorPage";
 import { BlogCard } from "../BlogCard";
-import BlogsCSS from "../CSSmodules/blogs.module.css"
+import BlogsCSS from "../CSSmodules/blogs.module.css";
 import MainHeading from "../MainHeading";
 
 export const Blog = () => {
   //change this according to where the strapi admin is hosted
-  const host = "http://localhost:1337";
-  const { loading, data, error } = useFetch(`${host}/api/blogs?populate=blogImage,authorInfo.profilePic`);
+  const host = "http://localhost:1338";
+  const { loading, data, error } = useFetch(
+    `${host}/api/blogs?populate=blogImage,authorInfo.profilePic`
+  );
 
   //loading and error displays
   if (loading) return <LoadingPage />;
   if (error) return <ErrorPage />;
 
   //testing (remove if seen)
-  console.log({data})
+  console.log({ data });
 
   //maping Blog card with strapi blogs data
-  return(
-  <React.Fragment>
-    <div className={BlogsCSS["blogPage"]}>
-    <MainHeading headingName={"Our Blogs"}/>
-    <div className={BlogsCSS["blogCardContainer"]}>
-      {data.data.map((blog) => {
-        const blogImgUrl = `${host}${blog.attributes.blogImage.data.attributes.formats.medium.url}`
-        const authorImgUrl = `${host}${blog.attributes.authorInfo.profilePic.data.attributes.formats.thumbnail.url}`
-        return(
-          <BlogCard
-            key={blog.attributes.id}
-            id={blog.id}
-            slug={blog.attributes.slug}
-            image={blogImgUrl}
-            title={blog.attributes.blogTitle}
-            body={blog.attributes.blogBody}
-            authorPP={authorImgUrl}
-            authorName={blog.attributes.authorInfo.nameOfAuthor}
-            publishDate={blog.attributes.authorInfo.dateOfUpload}
-          />
-        )
-      })}
-    </div>
-    </div>
-  </React.Fragment>
-  )
+  return (
+    <React.Fragment>
+      <div className={BlogsCSS["blogPage"]}>
+        <MainHeading headingName={"Our Blogs"} />
+        <div className={BlogsCSS["blogCardContainer"]}>
+          {data.data.map((blog) => {
+            const blogImgUrl = `${host}${blog.attributes.blogImage.data.attributes.formats.medium.url}`;
+            const authorImgUrl = `${host}${blog.attributes.authorInfo.profilePic.data.attributes.formats.thumbnail.url}`;
+            return (
+              <BlogCard
+                key={blog.attributes.id}
+                id={blog.id}
+                slug={blog.attributes.slug}
+                image={blogImgUrl}
+                title={blog.attributes.blogTitle}
+                body={blog.attributes.blogBody}
+                authorPP={authorImgUrl}
+                authorName={blog.attributes.authorInfo.nameOfAuthor}
+                publishDate={blog.attributes.authorInfo.dateOfUpload}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
-
--- src/components/Pages/IndividualBlog.jsx
+++ src/components/Pages/IndividualBlog.jsx
@@ -3,31 +3,47 @@ import { useParams } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import LoadingPage from "../LoadingPage";
 import ErrorPage from "../ErrorPage";
-import INDBLG from "../CSSmodules/individualBlog.module.css"
+import INDBLG from "../CSSmodules/individualBlog.module.css";
+import ReactMarkdown from "react-markdown";
+export const IndividualBlog = () => {
+  //change this according to where the strapi admin is hosted
+  const host = "http://localhost:1338";
+  const { id } = useParams();
+  const { loading, data, error } = useFetch(
+    `${host}/api/blogs/${id}?populate=blogImage,authorInfo.profilePic`
+  );
 
-export const IndividualBlog = () =>{
-    //change this according to where the strapi admin is hosted
-    const host = "http://localhost:1337"
-    const { id }= useParams();
-    const { loading, data, error } = useFetch(`${host}/api/blogs/${id}?populate=blogImage,authorInfo.profilePic`)
+  //loading and error displays
+  if (loading) return <LoadingPage />;
+  if (error) return <ErrorPage />;
 
-    //loading and error displays
-    if (loading) return <LoadingPage />;
-    if (error) return <ErrorPage />;
+  //test remove if seen
+  console.log(data);
 
-    //test remove if seen
-    console.log(data);
-
-    return(
-        <div className={INDBLG["individualBlogPage"]}>
-            <h1 className={INDBLG["blogTitle"]}>{data.data.attributes.blogTitle}</h1>
-            <div className={INDBLG["author"]}>
-                <img src={`${host}${data.data.attributes.authorInfo.profilePic.data.attributes.formats.thumbnail.url}`} className={INDBLG["authorPP"]} alt="author"></img>
-                <h6 className={INDBLG["authorName"]}>{data.data.attributes.authorInfo.nameOfAuthor}</h6>
-                <h6 className={INDBLG["uploadDate"]}>{data.data.attributes.authorInfo.dateOfUpload}</h6>
-            </div>
-            <img src={`${host}${data.data.attributes.blogImage.data.attributes.url}`} className={INDBLG["blogImage"]} alt="blog"></img>
-            <p className={INDBLG["blogBody"]}>{data.data.attributes.blogBody}</p>
-        </div>
-    )
+  return (
+    <div className={INDBLG["individualBlogPage"]}>
+      <h1 className={INDBLG["blogTitle"]}>{data.data.attributes.blogTitle}</h1>
+      <div className={INDBLG["author"]}>
+        <img
+          src={`${host}${data.data.attributes.authorInfo.profilePic.data.attributes.formats.thumbnail.url}`}
+          className={INDBLG["authorPP"]}
+          alt="author"
+        ></img>
+        <h6 className={INDBLG["authorName"]}>
+          {data.data.attributes.authorInfo.nameOfAuthor}
+        </h6>
+        <h6 className={INDBLG["uploadDate"]}>
+          {data.data.attributes.authorInfo.dateOfUpload}
+        </h6>
+      </div>
+      <img
+        src={`${host}${data.data.attributes.blogImage.data.attributes.url}`}
+        className={INDBLG["blogImage"]}
+        alt="blog"
+      ></img>
+      <p className={INDBLG["blogBody"]}>
+        <ReactMarkdown>{data.data.attributes.blogBody}</ReactMarkdown>
+      </p>
+    </div>
+  );
 };
