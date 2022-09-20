730a7078594c8fbdf530bd533d863162047a37fa
SDhakZ <sharnam34@gmail.com>
2022-09-20 14:38:35 +0545

    "Back button on blog"

--- src/components/Pages/IndividualBlog.jsx
+++ src/components/Pages/IndividualBlog.jsx
@@ -5,6 +5,7 @@ import LoadingPage from "../LoadingPage";
 import ErrorPage from "../ErrorPage";
 import INDBLG from "../CSSmodules/individualBlog.module.css";
 import ReactMarkdown from "react-markdown";
+import { Link } from "react-router-dom";
 export const IndividualBlog = () => {
   //change this according to where the strapi admin is hosted
   const host = "http://localhost:1338";
@@ -21,35 +22,43 @@ export const IndividualBlog = () => {
   console.log(data);
 
   return (
-    <div className={INDBLG["individualBlogPage"]}>
-      <div className={INDBLG["blog-headerContainer"]}>
-        <h1 className={INDBLG["blogTitle"]}>
-          {data.data.attributes.blogTitle}
-        </h1>
-        <div className={INDBLG["auth_date-container"]}>
+    <>
+      <div className={INDBLG["individualBlogPage"]}>
+        <div className={INDBLG["back-button-container"]}>
+          <Link to={{ pathname: "/blog" }} className={INDBLG["back-button"]}>
+            <i class="fa-sharp fa-solid fa-arrow-left"></i> Go Back
+          </Link>
+        </div>
+
+        <div className={INDBLG["blog-headerContainer"]}>
+          <h1 className={INDBLG["blogTitle"]}>
+            {data.data.attributes.blogTitle}
+          </h1>
+          <div className={INDBLG["auth_date-container"]}>
+            <img
+              src={`${host}${data.data.attributes.authorInfo.profilePic.data.attributes.formats.thumbnail.url}`}
+              className={INDBLG["authorPP"]}
+              alt="author"
+            ></img>
+            <h6 className={INDBLG["authorName"]}>
+              {data.data.attributes.authorInfo.nameOfAuthor}
+            </h6>
+            <h6 className={INDBLG["uploadDate"]}>
+              {data.data.attributes.authorInfo.dateOfUpload}
+            </h6>
+          </div>
+        </div>
+        <div className={INDBLG["ind-content"]}>
           <img
-            src={`${host}${data.data.attributes.authorInfo.profilePic.data.attributes.formats.thumbnail.url}`}
-            className={INDBLG["authorPP"]}
-            alt="author"
+            src={`${host}${data.data.attributes.blogImage.data.attributes.url}`}
+            className={INDBLG["blogImage"]}
+            alt="blog"
           ></img>
-          <h6 className={INDBLG["authorName"]}>
-            {data.data.attributes.authorInfo.nameOfAuthor}
-          </h6>
-          <h6 className={INDBLG["uploadDate"]}>
-            {data.data.attributes.authorInfo.dateOfUpload}
-          </h6>
+          <p className={INDBLG["blogBody"]}>
+            <ReactMarkdown>{data.data.attributes.blogBody}</ReactMarkdown>
+          </p>
         </div>
       </div>
-      <div className={INDBLG["ind-content"]}>
-        <img
-          src={`${host}${data.data.attributes.blogImage.data.attributes.url}`}
-          className={INDBLG["blogImage"]}
-          alt="blog"
-        ></img>
-        <p className={INDBLG["blogBody"]}>
-          <ReactMarkdown>{data.data.attributes.blogBody}</ReactMarkdown>
-        </p>
-      </div>
-    </div>
+    </>
   );
 };
