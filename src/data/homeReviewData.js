a5c0527232bdbeda882ba51ab2917bea93fb9908
Sanjay Malla <mallasanjay44@gmail.com>
2022-09-30 13:50:07 +0545

    added Review Sections

--- src/data/homeReviewData.js
+++ src/data/homeReviewData.js
@@ -20,5 +20,33 @@ const homecommentData = [
     comment:
       "All-in-one place for engaging in remote team discussions for collaborating and also tracking progress. With your team you can achieve more efficiently being online.",
   },
+  {
+    id: 4,
+    img: require("../assets/Images/Home-Image/altImage.jpg"),
+    name: "Kath Green",
+    comment:
+      "All-in-one place for engaging in remote team discussions for collaborating and also tracking progress. With your team you can achieve more efficiently being online.",
+  },
+  {
+    id: 5,
+    img: require("../assets/Images/Home-Image/altImage.jpg"),
+    name: "Kath Green",
+    comment:
+      "All-in-one place for engaging in remote team discussions for collaborating and also tracking progress. With your team you can achieve more efficiently being online.",
+  },
+  {
+    id: 6,
+    img: require("../assets/Images/Home-Image/altImage.jpg"),
+    name: "Kath Green",
+    comment:
+      "All-in-one place for engaging in remote team discussions for collaborating and also tracking progress. With your team you can achieve more efficiently being online.",
+  },
+  {
+    id: 7,
+    img: require("../assets/Images/Home-Image/altImage.jpg"),
+    name: "Kath Green",
+    comment:
+      "All-in-one place for engaging in remote team discussions for collaborating and also tracking progress. With your team you can achieve more efficiently being online.",
+  },
 ];
 export default homecommentData;
